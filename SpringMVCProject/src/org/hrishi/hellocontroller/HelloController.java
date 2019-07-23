3fa4e6d3172b6c88e924eca9ca338a05d884e3c4
hrishi2710 <hrishirules2710@gmail.com>
2019-07-23 12:00:58 +0530

    SpringMVC: Implemented annotation based controller

--- SpringMVCProject/src/org/hrishi/hellocontroller/HelloController.java
+++ SpringMVCProject/src/org/hrishi/hellocontroller/HelloController.java
@@ -1,6 +1,10 @@
 package org.hrishi.hellocontroller;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/*import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
@@ -16,5 +20,16 @@ public class HelloController extends AbstractController{
 		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
 		
 		return modelandview;
+	}
+}*/
+
+@Controller
+public class HelloController{
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg", "Hi!");
+		return model;
+		
 	}
 }
