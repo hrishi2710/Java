b275f8d9f6dfc56f02dce48ff2c28b6d0a6c4904
hrishi2710 <hrishirules2710@gmail.com>
2019-08-22 13:31:33 +0530

    SpringMVC: Added basic initbinder and webdatabinder

--- AdmissionProject/src/org/hrishi/studentadmissioncontroller/StudentAdmissionController.java
+++ AdmissionProject/src/org/hrishi/studentadmissioncontroller/StudentAdmissionController.java
@@ -3,6 +3,8 @@ package org.hrishi.studentadmissioncontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentAdmissionController {
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.setDisallowedFields(new String[] {"studentMobile"});
+	}
 	@RequestMapping(value = "/admissionForm.html", method = RequestMethod.GET)
 	public ModelAndView getAdmissionForm() {
 		ModelAndView model = new ModelAndView("AdmissionForm");
